4250480f0b5de25ca0c75bbd5ac4014500fbc365
jLilja.dev <jeanvitorlilja@outlook.com>
2025-03-28 05:20:19 -0300

    returning

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/site-dado/", // Adicione o nome do repositório aqui
+  base: "/Site-Dado/", // Adicione o nome do repositório aqui
 }));
