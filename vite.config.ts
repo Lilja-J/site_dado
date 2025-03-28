96df04fab672f1d1daa32b38cf2c3bb64bd85013
jLilja.dev <jeanvitorlilja@outlook.com>
2025-03-28 05:15:02 -0300

    trying to solve a bug

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Site-Dado/", // Adicione o nome do repositório aqui
+  base: "/site-dado/", // Adicione o nome do repositório aqui
 }));
