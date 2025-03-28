6095aa3677582d8494a4f8033eb494177821c128
jLilja.dev <jeanvitorlilja@outlook.com>
2025-03-28 18:00:31 -0300

    n

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // Use "./" para builds locais
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Site-Dado/",
-}));
+});
